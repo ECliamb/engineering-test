30cf8edfdc345091505d8524824b17671e5d16ef
ECliamb <liam.burgoyne@eurocamp.co.uk>
2023-07-15 14:03:24 +0100

    cors

--- apps/engineering/src/main.ts
+++ apps/engineering/src/main.ts
@@ -4,6 +4,11 @@ import { AppModule } from './app.module';
 
 async function bootstrap(): Promise<void> {
   const app = await NestFactory.create(AppModule);
+  app.enableCors({
+    allowedHeaders: ['content-type'],
+    origin: 'http://localhost:3001',
+    credentials: true,
+  });
   await bootstrapApp(app, 'Engineering', 'api');
 }
 
